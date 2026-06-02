6623ea5ae7b0466409fd264b45a346b0495e8dc7
Metric <134078516+mjasil@users.noreply.github.com>
2026-06-02 23:00:57 +0530

    Configure remote image patterns in next.config.mjs
    Added remote image patterns for external sources.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "upload.wikimedia.org" },
+      { protocol: "https", hostname: "logo.clearbit.com" },
+    ],
+  },
 };
 
 export default nextConfig;
